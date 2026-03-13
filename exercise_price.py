526811d4686587eaa7aa01c642e0c328169807b0
matecelaa <mcelaa@mail.austral.edu.ar>
2026-03-13 11:47:20 -0300

    Add files via upload

--- exercise_price.py
+++ exercise_price.py
@@ -9,3 +9,9 @@ def price():
     4. El precio final (subtotal + propina)
     """
     precio_base = 100
+    print(precio_base * 0.21)
+    print(precio_base + precio_base * 0.21)
+    print((precio_base + precio_base * 0.21) * 0.10)
+    print((precio_base + precio_base * 0.21) + (precio_base + precio_base * 0.21) * 0.10)
+
+#price()
